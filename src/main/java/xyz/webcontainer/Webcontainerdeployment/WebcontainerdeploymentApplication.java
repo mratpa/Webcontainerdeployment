c9d424e623cbb8b050eee2d109aecaaa146e2996
macharya <madhav_acharya@hotmail.com>
2021-06-13 12:39:42 -0400

    add new files

---
+++ src/main/java/xyz/webcontainer/Webcontainerdeployment/WebcontainerdeploymentApplication.java
@@ -0,0 +1,13 @@
+package xyz.webcontainer.Webcontainerdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebcontainerdeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebcontainerdeploymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/webcontainer/Webcontainerdeployment/WebcontainerdeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.webcontainer.Webcontainerdeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebcontainerdeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
